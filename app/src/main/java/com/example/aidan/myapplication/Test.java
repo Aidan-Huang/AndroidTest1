fd159275ff2bc02911302d1518610c0faaae192b
Aidan <huangye2016@ideal.sh.cn>
2016-05-13 16:09:09 +0800

    first

---
+++ app/src/main/java/com/example/aidan/myapplication/Test.java
@@ -0,0 +1,17 @@
+package com.example.aidan.myapplication;
+
+
+public class Test {
+
+    public void test(){
+
+        System.out.print("Hello world!");
+
+    }
+
+    public static void main(String[] args){
+
+        Test t = new Test();
+        t.test();
+    }
+}
